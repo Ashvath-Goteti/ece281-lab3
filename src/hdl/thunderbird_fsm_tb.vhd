cdea9f25c383eb48f75d236d9f6724e36cddce27
Goteti <c26ashvath.goteti@afacademy.af.edu>
2024-03-31 19:04:42 -0600

    Lab3

--- src/hdl/thunderbird_fsm_tb.vhd
+++ src/hdl/thunderbird_fsm_tb.vhd
@@ -58,16 +58,87 @@ architecture test_bench of thunderbird_fsm_tb is
 	
 	component thunderbird_fsm is 
 	  port(
+	            i_clk, i_reset  : in    std_logic;
+                i_left, i_right : in    std_logic;
+                o_lights_L      : out   std_logic_vector(2 downto 0);
+                o_lights_R      : out   std_logic_vector(2 downto 0)
 		
 	  );
 	end component thunderbird_fsm;
+	
+	
+    signal w_reset : std_logic := '0';
+    signal w_clk : std_logic := '0';
+    signal w_left : std_logic := '0';
+    signal w_right : std_logic := '0';
+    signal w_lights_L : std_logic_vector (2 downto 0) :="000" ;
+    signal w_lights_R : std_logic_vector (2 downto 0) := "000";
 
 	-- test I/O signals
 	
 	-- constants
-	
+    constant k_clk_period : time := 10 ns;
 	
 begin
+    uut: thunderbird_fsm port map (
+          i_reset => w_reset,
+          i_clk => w_clk,
+          o_lights_L => w_lights_L,
+          o_lights_R => w_lights_R,
+          i_left => w_left,
+          i_right => w_right
+        );
+    clk_proc : process
+	begin
+		w_clk <= '0';
+        wait for k_clk_period/2;
+		w_clk <= '1';
+		wait for k_clk_period/2;
+	end process;
+	
+	-- Simulation process
+	-- Use 220 ns for simulation
+	sim_proc: process
+	begin
+		-- sequential timing		
+		w_reset <= '1';
+		wait for k_clk_period*1;
+		  assert w_lights_L = "000" and w_lights_R = "000" report "bad reset" severity failure;
+		  
+	
+		
+		w_reset <= '0';
+		wait for k_clk_period*1;
+		
+		w_left <= '1';
+		w_right <= '0';
+		wait for k_clk_period;
+		  assert w_lights_L = "001" and w_lights_R = "000" report "bad reset" severity failure;
+		wait for k_clk_period;
+		  assert w_lights_L = "011" and w_lights_R = "000" report "bad reset" severity failure;
+		wait for k_clk_period;
+		  assert w_lights_L = "111" and w_lights_R = "000" report "bad reset" severity failure;
+		wait for k_clk_period;
+		  assert w_lights_L = "000" and w_lights_R = "000" report "bad reset" severity failure; 
+		wait for k_clk_period;
+		w_left <= '0';
+		w_right <= '1';
+		wait for k_clk_period;
+		  assert w_lights_R = "001" and w_lights_L = "000" report "bad reset" severity failure;
+        wait for k_clk_period;
+          assert w_lights_R = "011" and w_lights_L = "000" report "bad reset" severity failure;
+        wait for k_clk_period;
+          assert w_lights_R = "111" and w_lights_L = "000" report "bad reset" severity failure;
+        wait for k_clk_period;
+          assert w_lights_R = "000" and w_lights_L = "000" report "bad reset" severity failure;
+        wait for k_clk_period;
+        w_left <= '1';
+        w_right <= '1';
+        wait for k_clk_period;
+           assert w_lights_L = "111" and w_lights_R = "111" report "bad reset" severity failure;
+        
+		  
+		
 	-- PORT MAPS ----------------------------------------
 	
 	-----------------------------------------------------
@@ -80,5 +151,7 @@ begin
 	-- Test Plan Process --------------------------------
 	
 	-----------------------------------------------------	
-	
+	wait;
+	end process;
 end test_bench;
+
